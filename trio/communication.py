71f6a2cc530dac4fd80031842e01d657b4933522
Benjamin Labrecque <labrecque.benji@gmail.com>
2023-03-04 12:41:54 +0100

    Add channel communication example

--- trio/communication.py
+++ trio/communication.py
@@ -3,6 +3,7 @@ Trio provies open_memory_channel() to communicate, which
 acts similar to queue.Queue()
 """
 
+import time
 import trio
 from pydantic import BaseModel
 
@@ -11,15 +12,17 @@ class Message(BaseModel):
 
 
 async def main():
+    start = time.time()
     async with trio.open_nursery() as nursery:
         send_channel, receive_channel = trio.open_memory_channel(0)
         nursery.start_soon(producer, send_channel)
         nursery.start_soon(consumer, receive_channel)
+    print(time.time() - start)
 
 
 async def producer(send_channel):
     async with send_channel:
-        for i in range(10):
+        for i in range(100000):
             message = Message(index=i)
             print("Producer: ", id(message))
             await send_channel.send(message)
@@ -28,7 +31,6 @@ async def producer(send_channel):
 async def consumer(receive_channel):
     async with receive_channel:
         async for message in receive_channel:
-            print(type(message))
             print("Consumer: ", id(message))
             print(f"Message index: {message.index}")
 
