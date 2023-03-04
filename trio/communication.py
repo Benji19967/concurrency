eb27c60ad9b4cc8602caa11a31a594e2780dae52
Benjamin Labrecque <labrecque.benji@gmail.com>
2023-03-04 13:05:28 +0100

    Update communication example

--- trio/communication.py
+++ trio/communication.py
@@ -10,25 +10,42 @@ from pydantic import BaseModel
 class Message(BaseModel):
     index: int
 
+async def dummy():
+    print("Dummy")
 
 async def main():
+    print("Starting main")
     start = time.time()
     async with trio.open_nursery() as nursery:
         send_channel, receive_channel = trio.open_memory_channel(0)
         nursery.start_soon(producer, send_channel)
         nursery.start_soon(consumer, receive_channel)
+
+        print("Before sleep 1")
+        time.sleep(2)
+        print("After sleep 1")
+        nursery.start_soon(dummy)
+
+        print("Before sleep 2")
+        time.sleep(2)
+        print("After sleep 2")
+
+        # Nursery functions get start here
+
     print(time.time() - start)
 
 
 async def producer(send_channel):
+    print("Starting producer")
     async with send_channel:
-        for i in range(100000):
+        for i in range(3):
             message = Message(index=i)
             print("Producer: ", id(message))
             await send_channel.send(message)
 
 
 async def consumer(receive_channel):
+    print("Starting consumer")
     async with receive_channel:
         async for message in receive_channel:
             print("Consumer: ", id(message))
