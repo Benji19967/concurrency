6caeb2023d8be1a3d3bf1ea2e31e1b7d8ff16bcb
Benjamin Labrecque <labrecque.benji@gmail.com>
2023-03-04 10:00:34 +0100

    Add trio examples

---
+++ trio/main.py
@@ -0,0 +1,12 @@
+import trio
+
+async def double_sleep(x):
+    print("HEY!")
+    await trio.sleep(2 * x)
+    print("HO")
+    await trio.sleep(2 * x)
+    print("DONE")
+
+
+if __name__ == "__main__":
+    trio.run(double_sleep, 1)
