ccc6b6480af773e9894c7b5c4c4bd621af41da26
Benjamin Labrecque <labrecque.benji@gmail.com>
2023-04-16 17:05:23 +0200

    Introduce format error to test black github action

--- trio/long_task_results.py
+++ trio/long_task_results.py
@@ -11,8 +11,8 @@ import time
 from typing import List
 import trio
 
-
 async def long_task(task_id: int, results: List[str]):
+
     print(f"Starting long task {task_id}")
     await trio.sleep(2)
     print(f"Done long task {task_id}")
